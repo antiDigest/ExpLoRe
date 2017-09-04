c8b4e25b6949b8ceb13812ff18c0d1933b9905f7
alchemist009 <alchemist009@gmail.com>
2017-09-04 13:28:40 -0500

    Splash activity added
    * New Splash activity added in Android Manifest
    * Removed fragement from activity_main.xml

--- app/src/main/java/com/evamy/explore/MainActivity.java
+++ app/src/main/java/com/evamy/explore/MainActivity.java
@@ -2,9 +2,16 @@ package com.evamy.explore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private static final String TAG = "LoginActivity";
+    //private static final int REQUEST_SIGNUP = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/evamy/explore/SplashActivity.java
+++ app/src/main/java/com/evamy/explore/SplashActivity.java
@@ -1,13 +1,27 @@
 package com.evamy.explore;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    Thread t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 2000);
     }
 }
